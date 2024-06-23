1562e5619facbda768bcbfc7e75f0da4236e10f1
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-06-23 15:03:28 -0300

    feat: create calc

--- app/src/main/java/com/rodrigo/custoviagem/MainActivity.kt
+++ app/src/main/java/com/rodrigo/custoviagem/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.rodrigo.custoviagem
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rodrigo.custoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(p: View) {
+        calculate(p.id)
+    }
+
+    fun calculate(id: Int){
+        if (id == R.id.button_calculate){
+
+            var distance = binding.editDistance.text.toString().toFloat()
+            var price = binding.editPreco.text.toString().toFloat()
+            var autonomia = binding.editAutonomia.text.toString().toFloat()
+
+            var calc = (distance * price) / autonomia
+
+            binding.textTotal.text = "R$${calc}0"
+
         }
     }
+
+
 }
